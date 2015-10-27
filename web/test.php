2d33be492f2c4a083594aee5df8c4fb3c52adc23
KokLip <lipzai0625@gmail.com>
2015-10-27 18:24:55 +0800

    dashboard added

---
+++ web/test.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/dashboard.css" rel="stylesheet">
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">GST</a>
+		</div>        
+	  </div>
+	</nav>
+	<div class="container-fluid">
+	  <div class="row">
+		<div class="col-md-2 sidebar">
+		  <ul class="nav nav-sidebar">
+			<li class="active"><a href="#">Admin</span></a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">Customers</a></li>
+			<li><a href="#">Purchase Order</a></li>		  
+			<li><a href="#">Quotation</a></li>
+			<li><a href="#">Delivery Order</a></li>
+			<li><a href="#">Invoice</a></li>
+			<li><a href="#">Credit Note</a></li>
+			<li><a href="#">Debit Note</a></li>		  
+			<li><a href="#">Payment Voucher</a></li>
+			<li><a href="#">Invoice Statement</a></li>
+			<li><a href="#">Logout</a></li>
+		  </ul>
+		</div>	
+	  </div>
+	</div>
+</body>
+</html>
